14a39bb2253e0ac598e0c82074a74ac1d8243a6d
Dmitrandir <dim9107@mail.ru>
2022-10-19 21:50:45 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Дмитрий Солопов on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
